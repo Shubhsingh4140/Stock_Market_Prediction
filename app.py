e0e5f0fa3fa0e64ec6a5e33f05b9cfb635d4f04f
Shubhankar Singh Sisodia <shubhankarsisodia@email.com>
2025-12-25 18:12:26 +0530

    Allow loading legacy model with Keras 3 safe_mode disabled

--- app.py
+++ app.py
@@ -76,7 +76,13 @@ data_training_array = scaler.fit_transform(data_training.values.reshape(-1, 1))
 # Load the trained model
 # model = load_model('keras_model.h5')
 # model = load_model("keras_model.h5", compile=False)
-model = load_model("keras_model_new.keras", compile=False)
+# model = load_model("keras_model_new.keras", compile=False)
+model = load_model(
+    "keras_model_new.keras",
+    compile=False,
+    safe_mode=False
+)
+
 
 
 
